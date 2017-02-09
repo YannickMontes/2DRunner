31ed1410e5b49899a5cc616d396615753e9dd6c1
YannickMontes <montes.yannick@gmail.com>
2017-02-09 16:25:57 +0100

    Add different layer to know where is the ground. Add scripts, playercontroller scripts. Now the player moove forward. BoardManager script generate a basic line of floor.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    public int blocks;
+    public GameObject background;
+    public GameObject[] floor;
+	// Use this for initialization
+	void Start ()
+    {
+        Instantiate(background);
+	    for(int i=0; i<blocks; i++)
+        {
+            if(i==0)
+            {
+                Instantiate(floor[0], new Vector3(i*1.28f,0.0f,0.0f), Quaternion.identity);
+            }
+            else if(i==blocks-1)
+            {
+                Instantiate(floor[2], new Vector3(i * 1.28f, 0.0f, 0.0f), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(floor[1], new Vector3(i * 1.28f, 0.0f, 0.0f), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public bool grounded;
+    public LayerMask ground;
+
+    private Collider2D playerCollider;
+
+    private Rigidbody2D playerBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.playerBody = GetComponent<Rigidbody2D>();
+        this.playerCollider = GetComponent<Collider2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        grounded = Physics2D.IsTouchingLayers(this.playerCollider, ground);
+
+        playerBody.velocity = new Vector2(moveSpeed, playerBody.velocity.y);
+        
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, jumpForce);
+        }
+	}
+}

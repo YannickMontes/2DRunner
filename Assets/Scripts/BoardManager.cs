07cd84bdc123a11ee95b236d417bcb0e830e4aa2
YannickMontes <montes.yannick@gmail.com>
2017-02-13 16:26:19 +0100

    Add a condition to disable generation in editor

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -8,25 +8,29 @@ public class BoardManager : MonoBehaviour
     public int blocks;
     public GameObject background;
     public GameObject[] floor;
+    public bool generate;
 	// Use this for initialization
 	void Start ()
     {
-        Instantiate(background);
-	    for(int i=0; i<blocks; i++)
+        if (generate)
         {
-            if(i==0)
+            Instantiate(background);
+            for (int i = 0; i < blocks; i++)
             {
-                Instantiate(floor[0], new Vector3(i,0.0f,0.0f), Quaternion.identity);
+                if (i == 0)
+                {
+                    Instantiate(floor[0], new Vector3(i, 0.0f, 0.0f), Quaternion.identity);
+                }
+                else if (i == blocks - 1)
+                {
+                    Instantiate(floor[2], new Vector3(i, 0.0f, 0.0f), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(floor[1], new Vector3(i, 0.0f, 0.0f), Quaternion.identity);
+                }
             }
-            else if(i==blocks-1)
-            {
-                Instantiate(floor[2], new Vector3(i, 0.0f, 0.0f), Quaternion.identity);
-            }
-            else
-            {
-                Instantiate(floor[1], new Vector3(i, 0.0f, 0.0f), Quaternion.identity);
-            }
-        }
+        }   
 	}
 	
 	// Update is called once per frame

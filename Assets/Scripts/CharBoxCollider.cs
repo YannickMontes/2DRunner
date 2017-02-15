4bb68731dd0436211c963a9dda162a0b5cadc7ff
YannickMontes <montes.yannick@gmail.com>
2017-02-15 17:37:02 +0100

    Add a script to handle player extreme collider
    In order to apply the gravity again on the player when he hit an
    obstacle on the way

---
+++ Assets/Scripts/CharBoxCollider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharBoxCollider : MonoBehaviour
+{
+    private PlayerController player;
+
+    public void Start()
+    {
+        this.player = this.gameObject.GetComponentInParent<PlayerController>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        player.extremeColliding = true;
+    }
+
+    public void OnCollisionExit2D(Collision2D coll)
+    {
+        player.extremeColliding = false;
+    }
+}

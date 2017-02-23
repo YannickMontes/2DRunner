80583bbf123226ff5c81e74232559d167c9e6b6e
YannickMontes <montes.yannick@gmail.com>
2017-02-23 13:06:22 +0100

    Add the possibility to speed up or down
    The player can now press arrow keys in order to go faster or slower
    Add a moveSpeedMax variable
    Put the treatment of each different input in a proper function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed;
+    public float moveSpeedMax = 20;
     public float jumpForce;
 
+    private float moveSpeed;
+    private float speedCoeff = 7;
+
     public bool grounded;
-    public bool extremeColliding;
+    public bool extremeColliding; // To detect if the player colide with someting in front of him
     public LayerMask ground;
 
     private Collider2D playerCollider;
@@ -18,6 +21,7 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        this.moveSpeed = this.moveSpeedMax;
         this.playerBody = GetComponent<Rigidbody2D>();
         this.playerCollider = GetComponent<Collider2D>();
         this.playerAnimator = GetComponent<Animator>();
@@ -35,13 +39,43 @@ public class PlayerController : MonoBehaviour {
 
         //playerBody.velocity = new Vector2(moveSpeed, playerBody.velocity.y);
 
+        this.HandleInputs();
+
+        this.UpdateAnimatorVariables();
+	}
+
+    private void HandleInputs()
+    {
+        this.InputJump();
+        this.InputSpeed();
+    }
+
+    private void InputJump()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             playerBody.AddForce(Vector2.up * jumpForce);
         }
+    }
 
-        this.UpdateAnimatorVariables();
-	}
+    private void InputSpeed()
+    {
+        if(Input.anyKey)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                this.moveSpeed = this.moveSpeedMax - speedCoeff;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                this.moveSpeed = this.moveSpeedMax + speedCoeff;
+            }
+        }
+        else
+        {
+            this.moveSpeed = this.moveSpeedMax;
+        }
+    }
 
     void FixedUpdate()
     {

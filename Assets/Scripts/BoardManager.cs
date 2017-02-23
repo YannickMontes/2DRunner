c78d584bf9202340cc7ac1b48eaac9e564f944af
YannickMontes <montes.yannick@gmail.com>
2017-02-23 13:07:42 +0100

    Add the generation of the background (infinite)
    Now the background is generated with the chunks
    Refactor the code with more accurate funciton name & Generate global
    function add a specific generate for each propre thing (background,
    chunks and after objects)
    Same for destroy functions

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,6 +5,7 @@ public class BoardManager : MonoBehaviour
 {
     public bool allowGeneration;
     public GameObject[] chunks;
+    public GameObject background;
     public Transform playerPosition;
     public int chunkSize;
 
@@ -12,39 +13,68 @@ public class BoardManager : MonoBehaviour
     public int beginGeneration = 0;
     private int endGeneration = 0;
     private List<GameObject> chunksGenerated;
+    private List<GameObject> backgroundGenerated;
 	// Use this for initialization
 	void Start ()
     {
         chunksGenerated = new List<GameObject>();
-        GenerateTerrain();
+        backgroundGenerated = new List<GameObject>();
+        Generate();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         generate = (endGeneration - playerPosition.position.x < chunkSize ? true : false);
-        GenerateTerrain();
-        DeleteOldTerrain();
+        Generate();
+        DestroyOld();
 	}
 
-    private void DeleteOldTerrain()
+    private void Generate()
     {
-        if(playerPosition.position.x - beginGeneration > chunkSize * 2)
+        if (allowGeneration && generate)
         {
-            GameObject tmp = chunksGenerated[0];
-            chunksGenerated.RemoveAt(0);
-            Destroy(tmp);
+            this.GenerateBackground();
+            this.GenerateChunks();
+            endGeneration += chunkSize;
+        }
+    }
+
+    private void GenerateChunks()
+    {
+        GameObject chunkTmp = (GameObject)Instantiate(chunks[0], new Vector3(endGeneration, 0.0f, 0.0f), Quaternion.identity);
+        chunksGenerated.Add(chunkTmp);
+    }
+
+    private void GenerateBackground()
+    {
+        GameObject bgTmp = (GameObject)Instantiate(background, new Vector3(endGeneration + ((chunkSize-1) / 2.0f), 10.0f, 0.0f), Quaternion.identity);
+        backgroundGenerated.Add(bgTmp);
+    }
+
+    private void DestroyOld()
+    {
+        if (playerPosition.position.x - beginGeneration > chunkSize * 2)
+        {
+            this.DestroyOldBackground();
+            this.DestroyOldChunks();
             beginGeneration += chunkSize;
         }
     }
 
-    public void GenerateTerrain()
-    {
-        if(allowGeneration && generate)
-        {
-            GameObject tmp = (GameObject)Instantiate(chunks[0], new Vector3(endGeneration, 0.0f, 0.0f), Quaternion.identity);
-            chunksGenerated.Add(tmp);
-            endGeneration += chunkSize;
-        }
+    private void DestroyOldChunks()
+    { 
+        GameObject chunkTmp = chunksGenerated[0];
+        chunksGenerated.RemoveAt(0);
+        Destroy(chunkTmp);
     }
+
+    private void DestroyOldBackground()
+    {
+        GameObject bgTmp = backgroundGenerated[0];
+        backgroundGenerated.RemoveAt(0);
+        Destroy(bgTmp);
+    }
+
+    
 }

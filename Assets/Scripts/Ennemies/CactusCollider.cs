09610ef2c40bebf9135eedb7b13363d744ef676f
MONTES Yannick <montes.yannick@gmail.com>
2017-11-05 20:02:01 -0500

    Implement damage when colliding cactus

---
+++ Assets/Scripts/Ennemies/CactusCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D collider)
+	{
+		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerStats> ().DecreaseLife ();
+	}
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	private int life;
+	private bool vulnerable;
+
+	// Use this for initialization
+	void Start () 
+	{
+		life = 3;	
+		vulnerable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (life == 0) 
+		{
+			//End the game
+		}
+	}
+
+	public void DecreaseLife()
+	{
+		if (vulnerable) 
+		{
+			life--;
+			StartCoroutine (Fade ());
+			vulnerable = false;
+		}
+	}
+
+	public IEnumerator Fade() {
+		for (int i = 0; i < 12; i++) {
+			this.GetComponent<SpriteRenderer> ().enabled = !this.GetComponent<SpriteRenderer> ().enabled;
+			yield return new WaitForSeconds (.05f);
+		}
+		vulnerable = true;
+	}
+}

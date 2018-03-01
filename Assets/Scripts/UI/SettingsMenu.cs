cd51b83e6e8fdfad1deac982b01d87b5a043a65c
YannickMontes <montes.yannick@gmail.com>
2018-03-01 19:15:54 +0100

    Main & Settings menu ok

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    public Resolution[] resolutions;
+
+    public void Start()
+    {
+        this.resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> resolutionsString = new List<string>();
+        int currentResolution = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            resolutionsString.Add(resolutions[i].width + " * " + resolutions[i].height);
+            if (Screen.height == resolutions[i].height && Screen.width == resolutions[i].width)
+            {
+                currentResolution = i;
+            }
+        }
+        resolutionDropdown.AddOptions(resolutionsString);
+        resolutionDropdown.value = currentResolution;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        //TO UNCOMMENT WHEN SOUND IS READY
+        //audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
